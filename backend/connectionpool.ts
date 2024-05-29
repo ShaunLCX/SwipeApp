2b759d0d1e2648b5443a5a162e843351027069a1
Shaun Lee <shaunlcx@outlook.com>
2024-05-29 23:33:36 +0800

    commit

--- backend/connectionpool.ts
+++ backend/connectionpool.ts
@@ -1,10 +1,10 @@
 import sql from 'mssql';
 
 const pool = new sql.ConnectionPool({
-    user: 'shaun3', // Replace 'your_sql_server_username' with your actual SQL Server username
-    password: 'admin', // Replace 'your_sql_server_password' with your actual SQL Server password
-    server: 'DESKTOP-KJG0MS7',
-    database: 'SwipeAppDb',
+    user: 'shaun3', // Replace 'your_sql_server_username' 
+    password: 'admin', // Replace 'your_sql_server_password' 
+    server: 'DESKTOP-KJG0MS7',  // Server name
+    database: 'SwipeAppDb',    // DB NAME
     options: {
         encrypt: true, // Use this if you're on Windows Azure
         trustServerCertificate: true, // Use this if you're on a local development environment
